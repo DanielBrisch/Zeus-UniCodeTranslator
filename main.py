bbc0fdc708643928500d17bfb9f1a616413a9814
DanielBrisch <danielbrisch157@gmail.com>
2023-09-23 18:06:30 -0300

    02

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ rotulo.pack()
 entrada = tk.Entry(janela, width=50)
 entrada.pack()
 
-label_resultado = tk.Label(janela, text="")
-label_resultado.pack()
+    # try:
+    #     valor_convertido = float(valor_inserido)
+    #     label_resultado.config(text=f"Valor inserido: {valor_convertido}")
+    # except ValueError:
+    #     label_resultado.config(text="Insira um valor numérico válido.")
 
+label_resultado = tk.Label(janela)
+ 
 def exibir_valor():
-    valor_inserido = entrada.get()
-    try:
-        valor_convertido = float(valor_inserido)
-        label_resultado.config(text=f"Valor inserido: {valor_convertido}")
-    except ValueError:
-        label_resultado.config(text="Insira um valor numérico válido.")
+    valor_inserido = entrada.get().strip() 
+    if 'é' in valor_inserido:
+        valor_modificado = valor_inserido.replace('é', '\\u00e1')
+        label_resultado.config(text=f"Texto modificado: {valor_modificado}")
+    else:
+        label_resultado.config(text="Valor nao possui UniCode")
 
 botao = tk.Button(janela, text="Exibir Valor", command=exibir_valor)
-botao.pack()
+botao.pack(pady=20)
+
+label_resultado.pack()
 
 janela.mainloop()
